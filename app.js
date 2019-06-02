e2d79c5e8a66d870949ab78f8a0efc0beb488881
Nick-Hericks <nickhericks@gmail.com>
2019-06-02 10:55:31 +0200

    Fix app.js formatting

--- app.js
+++ app.js
@@ -17,28 +17,28 @@ app.use(morgan('dev'));
 
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the REST API project!',
-  });
+	res.json({
+		message: 'Welcome to the REST API project!',
+	});
 });
 
 // send 404 if no other route matched
 app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route Not Found',
-  });
+	res.status(404).json({
+		message: 'Route Not Found',
+	});
 });
 
 // setup a global error handler
 app.use((err, req, res, next) => {
-  if (enableGlobalErrorLogging) {
-    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
-  }
+	if (enableGlobalErrorLogging) {
+		console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+	}
 
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: {},
-  });
+	res.status(err.status || 500).json({
+		message: err.message,
+		error: {},
+	});
 });
 
 // set our port
@@ -46,5 +46,5 @@ app.set('port', process.env.PORT || 5000);
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
+	console.log(`Express server is listening on port ${server.address().port}`);
 });
