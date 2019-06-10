9d0ffbbeae93b29276a326850647b3c86742662d
Nick-Hericks <nickhericks@gmail.com>
2019-06-10 17:24:13 +0200

    Define user.js model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define('User', {
+		id: DataTypes.INTEGER,
+		firstName: DataTypes.STRING,
+		lastName: DataTypes.STRING,
+		emailAddress: DataTypes.STRING,
+		password: DataTypes.STRING
+	}, {});
+	User.associate = function(models) {
+		// associations can be defined here
+	};
+	return User;
+};
