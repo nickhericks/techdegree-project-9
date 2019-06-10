a0853d5c24973939c282b2687ff0f7a7c7a22378
Nick-Hericks <nickhericks@gmail.com>
2019-06-10 18:16:11 +0200

    Add course model validation

--- models/course.js
+++ models/course.js
@@ -1,14 +1,45 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Course = sequelize.define('Course', {
-    id: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    estimatedTime: DataTypes.STRING,
-    materialsNeeded: DataTypes.STRING
-  }, {});
-  Course.associate = function(models) {
-    // associations can be defined here
-  };
-  return Course;
+	const Course = sequelize.define(
+		'Course',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: 'Please enter a title'
+					}
+				}
+			},
+			description: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: 'Please enter a description'
+					}
+				}
+			},
+			estimatedTime: DataTypes.STRING,
+			materialsNeeded: DataTypes.STRING
+		},
+		{}
+	);
+	Course.associate = function(models) {
+		// associations can be defined here
+
+
+
+
+
+
+
+	};
+	return Course;
 };
