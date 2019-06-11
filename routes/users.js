566eb2148fc817124e07c4679668d518afeba5c1
Nick-Hericks <nickhericks@gmail.com>
2019-06-11 11:45:11 +0200

    Add create user route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const User = require('../models').User;
+const { check, validationResult } = require('express-validator/check');
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
 
 const router = express.Router();
 
@@ -12,11 +15,49 @@ router.get('/', (req, res) => {
 
 // POST /api/users 201
 // Creates a user, sets the Location header to "/", and returns no content
-router.post('/', (req, res) => {
-	res.json({ greeting: 'user routes' });
-});
+router.post('/', [
+	check('firstName')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "first name"'),
+	check('lastName')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "last name"'),
+	check('email')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "email"')
+		.isEmail()
+		.withMessage('Please provide a valid email address for "email"'),
+	check('password')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "password"')
+		.isLength({ min: 8, max: 20 })
+		.withMessage('Please provide a value for "password" that is between 8 and 20 characters in length'),
+],
+(req, res) => {
+	// Attempt to get the validation result from the Request object.
+	const errors = validationResult(req);
 
+	// If there are validation errors...
+	if (!errors.isEmpty()) {
+		// Use the Array `map()` method to get a list of error messages.
+		const errorMessages = errors.array().map(error => error.msg);
+		// Return the validation errors to the client.
+		return res.status(400).json({ errors: errorMessages });
+	}
+	// Get the user from the request body.
+	const user = req.body;
 
+	// Hash the new user's password.
+	user.password = bcryptjs.hashSync(user.password);
+
+	// Add the user to the `users` array.
+	// users.push(user);
+	User.create();
+
+	// Set the status to 201 Created and end the response.
+	res.status(201).end();
+}
+);
 
 
 
