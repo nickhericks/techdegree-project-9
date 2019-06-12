56b13cc0120f9d4a98cb473732a70a52870ccf92
Nick-Hericks <nickhericks@gmail.com>
2019-06-12 13:14:39 +0200

    Add authentication

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,78 @@ const auth = require('basic-auth');
 
 const router = express.Router();
 
+
+
+const authenticateUser = async (req, res, next) => {
+  let message = null;
+
+  // Parse the user's credentials from the Authorization header.
+  const credentials = auth(req);
+
+  // If the user's credentials are available...
+  // Attempt to retrieve the user from the data store
+  // by their username (i.e. the user's "key"
+  // from the Authorization header).
+  if (credentials) {
+
+		const user = await User.findOne({emailAddress: credentials.name});
+
+		// If a user was successfully retrieved from the data store...
+		// Use the bcryptjs npm package to compare the user's password
+		// (from the Authorization header) to the user's password
+		// that was retrieved from the data store.
+		if (user) {
+			const authenticated = bcryptjs.compareSync(
+				credentials.pass,
+				user.password
+			);
+
+			// If the passwords match...
+			// Then store the retrieved user object on the request object
+			// so any middleware functions that follow this middleware function
+			// will have access to the user's information.
+			if (authenticated) {
+				console.log(`Authentication successful for username: ${user.username}`);
+				// Set found user data to variable to be used later in routes
+				req.currentUser = user;
+			} else {
+				message = `Authentication failure for username: ${user.username}`;
+			}
+		} else {
+			message = `User not found for username: ${credentials.emailAddress}`;
+		}
+  } else {
+    message = "Auth header not found";
+  }
+
+  // If user authentication failed...
+  if (message) {
+    console.log(message);
+    // Return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({ message: "Access Denied" });
+  } else {
+    // Or if user authentication succeeded...
+    // Call the next() method.
+    next();
+  }
+};
+
+
+
+
 // GET /api/users 200
 // return the currently authenticated user
-router.get('/', (req, res) => {
-	res.json({ greeting: 'user routes' });
+router.get("/", authenticateUser, (req, res) => {
+  const user = req.currentUser;
+
+  res.json({ user });
 });
 
 
+
+
+
+// TODO: Check existing email address
 // POST /api/users 201
 // Creates a user, sets the Location header to "/", and returns no content
 router.post('/', [
