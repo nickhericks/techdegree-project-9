1edfeffbebc238fc7ba645fd0ee597da08f5c9a3
Nick-Hericks <nickhericks@gmail.com>
2019-06-12 20:58:28 +0200

    Update user comments

--- routes/users.js
+++ routes/users.js
@@ -24,11 +24,9 @@ function asyncHandler(cb) {
 // GET /api/users 200
 // return the currently authenticated user
 router.get("/", authenticateUser, (req, res) => {
-  const user = req.currentUser;
   const { firstName, lastName, emailAddress } = req.currentUser;
   res.json({ firstName, lastName, emailAddress });
 });
-
 
 
 // POST /api/users 201
@@ -50,49 +48,49 @@ router.post('/', [
 		.withMessage('Please provide a value for "password"')
 		.isLength({ min: 8, max: 20 })
 		.withMessage('Please provide a value for "password" that is between 8 and 20 characters in length'),
-],
-asyncHandler( async (req, res) => {
-	// Attempt to get the validation result from the Request object.
-	const errors = validationResult(req);
+	],
+	asyncHandler( async (req, res) => {
+		// Attempt to get the validation result from the Request object.
+		const errors = validationResult(req);
 
-	// If there are validation errors...
-	if (!errors.isEmpty()) {
-		// Use the Array `map()` method to get a list of error messages.
-		const errorMessages = errors.array().map(error => error.msg);
-		// Return the validation errors to the client.
-		return res.status(400).json({ errors: errorMessages });
-	}
-	// Get the user from the request body.
-	const user = req.body;
-	
-	// const existingEmail = user.emailAddress;
-
-	const existingUser = await User.findOne({
-		where: {
-			emailAddress: user.emailAddress
+		// If there are validation errors...
+		if (!errors.isEmpty()) {
+			// Use the Array `map()` method to get a list of error messages.
+			const errorMessages = errors.array().map(error => error.msg);
+			// Return the validation errors to the client.
+			return res.status(400).json({ errors: errorMessages });
 		}
-	});
-
-	if (!existingUser) {
-
-		// Hash the new user's password.
-		user.password = bcryptjs.hashSync(user.password);
-
-		// Create user
-		User.create({
-			firstName: user.firstName,
-			lastName: user.lastName,
-			emailAddress: user.emailAddress,
-			password: user.password
+		// Get the user from the request body.
+		const user = req.body;
+		
+		// check if user already exists in Users table
+		const existingUser = await User.findOne({
+			where: {
+				emailAddress: user.emailAddress
+			}
 		});
 
-		// Set the status to 201 Created and end the response.
-		res.location('/').status(201).end();
-	} else {
-		res.status(400).json({ message: "Email address already exists" });
-	}
-}));
+		// create new user if not already in Users table
+		if (!existingUser) {
 
+			// Hash the new user's password.
+			user.password = bcryptjs.hashSync(user.password);
+
+			// Create user
+			User.create({
+				firstName: user.firstName,
+				lastName: user.lastName,
+				emailAddress: user.emailAddress,
+				password: user.password
+			});
+
+			// Set the status to 201 Created and end the response.
+			res.location('/').status(201).end();
+		} else {
+			res.status(400).json({ message: "Email address already exists" });
+		}
+	}
+));
 
 
 module.exports = router;
