1b0e178927a42651be92a7d33f3f4dd60f32cf6f
Nick-Hericks <nickhericks@gmail.com>
2019-06-12 19:42:03 +0200

    Create authenticateUser.js file

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const express = require('express');
 const Course = require('../models').Course;
 const User = require('../models').User;
+const authenticateUser = require("./authenticate");
 const { check, validationResult } = require("express-validator/check");
 // const { authenticateUser } = require("./users");
 const bcryptjs = require("bcryptjs");
@@ -21,68 +24,10 @@ function asyncHandler(cb) {
 }
 
 
-const authenticateUser = asyncHandler( async (req, res, next) => {
-  let message = null;
-
-  // Parse the user's credentials from the Authorization header.
-  const credentials = auth(req);
-
-  // If the user's credentials are available...
-  // Attempt to retrieve the user from the data store
-  // by their username (i.e. the user's "key"
-  // from the Authorization header).
-  if (credentials) {
-    const user = await User.findOne({
-      where: {
-        emailAddress: credentials.name
-      }
-    });
-
-    // If a user was successfully retrieved from the data store...
-    // Use the bcryptjs npm package to compare the user's password
-    // (from the Authorization header) to the user's password
-    // that was retrieved from the data store.
-    if (user) {
-      const authenticated = bcryptjs.compareSync(
-        credentials.pass,
-        user.password
-      );
-
-      // If the passwords match...
-      // Then store the retrieved user object on the request object
-      // so any middleware functions that follow this middleware function
-      // will have access to the user's information.
-      if (authenticated) {
-        console.log(`Authentication successful for username: ${user.username}`);
-        // Set found user data to variable to be used later in routes
-        req.currentUser = user;
-      } else {
-        message = `Authentication failure for username: ${user.username}`;
-      }
-    } else {
-      message = `User not found for username: ${credentials.emailAddress}`;
-    }
-  } else {
-    message = "Auth header not found";
-  }
-
-  // If user authentication failed...
-  if (message) {
-    console.log(message);
-    // Return a response with a 401 Unauthorized HTTP status code.
-    res.status(401).json({ message: "Access Denied" });
-  } else {
-    // Or if user authentication succeeded...
-    // Call the next() method.
-    next();
-  }
-});
-
 
 // GET /api/courses 200
 // Returns a list of courses (including the user that owns each course)
 router.get('/', asyncHandler( async (req, res) => {
-	// throw new Error('Oh noooooooo!');
 	const courses = await Course.findAll({
 		attributes: ["id", "title", "description", "userId"],
 		include: [
@@ -99,7 +44,6 @@ router.get('/', asyncHandler( async (req, res) => {
 // GET /api/courses/:id 200
 // Returns a course (including the user that owns the course) for the provided course ID
 router.get('/:id', asyncHandler( async (req, res) => {
-	// throw new Error('Oh noooooooo!');
 	const course = await Course.findByPk(req.params.id, {
 		attributes: ["id", "title", "description", "userId"],
 		include: [
