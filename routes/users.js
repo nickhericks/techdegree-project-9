e69a38c2829f5420bb00b04d08851327642c8c54
Nick-Hericks <nickhericks@gmail.com>
2019-06-12 18:56:34 +0200

    add where option to auth middleware

--- routes/users.js
+++ routes/users.js
@@ -31,32 +31,38 @@ const authenticateUser = asyncHandler( async (req, res, next) => {
   // by their username (i.e. the user's "key"
   // from the Authorization header).
   if (credentials) {
-		const user = await User.findOne({emailAddress: credentials.name});
+    const user = await User.findOne({
+      where: {
+        emailAddress: credentials.name
+      }
+    });
 
-		// If a user was successfully retrieved from the data store...
-		// Use the bcryptjs npm package to compare the user's password
-		// (from the Authorization header) to the user's password
-		// that was retrieved from the data store.
-		if (user) {
-			const authenticated = bcryptjs.compareSync(
-				credentials.pass,
-				user.password
-			);
+    // If a user was successfully retrieved from the data store...
+    // Use the bcryptjs npm package to compare the user's password
+    // (from the Authorization header) to the user's password
+    // that was retrieved from the data store.
+    if (user) {
+      const authenticated = bcryptjs.compareSync(
+        credentials.pass,
+        user.password
+      );
 
-			// If the passwords match...
-			// Then store the retrieved user object on the request object
-			// so any middleware functions that follow this middleware function
-			// will have access to the user's information.
-			if (authenticated) {
-				console.log(`Authentication successful for username: ${user.username}`);
-				// Set found user data to variable to be used later in routes
-				req.currentUser = user;
-			} else {
-				message = `Authentication failure for username: ${user.username}`;
-			}
-		} else {
-			message = `User not found for username: ${credentials.emailAddress}`;
-		}
+      // If the passwords match...
+      // Then store the retrieved user object on the request object
+      // so any middleware functions that follow this middleware function
+      // will have access to the user's information.
+      if (authenticated) {
+        console.log(
+          `Authentication successful for username: ${user.username}`
+        );
+        // Set found user data to variable to be used later in routes
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.emailAddress}`;
+    }
   } else {
     message = "Auth header not found";
   }
